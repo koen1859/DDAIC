aef06c5321a9dfb3dcbd05fdcec91d814afddde3
koenstevens <gitlab.slurp563@passmail.net>
2026-04-28 10:43:02 +0200

    flake + python files

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "C++ flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      python =
+        pkgs.python313.withPackages
+        (ps:
+          with ps; [
+            ipython
+            numpy
+            matplotlib
+            polars
+          ]);
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = python;
+      };
+    });
+}

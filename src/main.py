f129bb550d8c6eb8451212e2b099d0bf25f923e7
koenstevens <gitlab.slurp563@passmail.net>
2026-04-29 11:58:27 +0200

    Clean up data import

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import polars as pl
 
-article_data: pl.DataFrame = pl.read_excel(
-    "data/DDAIC_ass_data_2026.xlsx", sheet_name="ArticleData"
-)
-demand_data: pl.DataFrame = pl.read_excel(
-    "data/DDAIC_ass_data_2026.xlsx", sheet_name="Demand data", has_header=True
-)
-demand_data.columns = [demand_data.columns[0], demand_data.columns[1]] + [
-    str(demand_data[0, i]) for i in range(2, demand_data.width)
-]
-demand_data.slice(1)
+article_data = pl.read_excel("data/DDAIC_ass_data_2026.xlsx", sheet_name="ArticleData")
+demand_data = pl.read_excel("data/DDAIC_ass_data_2026.xlsx", sheet_name="Demand data")
+demand_data = demand_data.rename(
+    {
+        demand_data.columns[i]: str(demand_data[0, i])
+        for i in range(2, demand_data.width)
+    }
+).slice(1)
+
+print(article_data)
+print(demand_data)
